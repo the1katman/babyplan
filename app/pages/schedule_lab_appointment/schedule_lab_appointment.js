2ac3b283e7f75a72f98b4fe3cb019dcf343218e0
Jason K <jkat@osx>
2014-08-09 23:03:46 -0700

    Changed the button time selection styling on the schedule lab appointment page.

--- app/pages/schedule_lab_appointment/schedule_lab_appointment.js
+++ app/pages/schedule_lab_appointment/schedule_lab_appointment.js
@@ -12,8 +12,10 @@ angular
                         var selectedHours = 8;
 
                         var currentHighlightedDate;
+                        var currentHighlightedTime;
 
-                        var highlightedClass = 'today';
+                        var highlightedDateClass = 'today';
+                        var highlightedTimeClass = 'now';
 
                         $scope.selectDate = function (day, month, year) {
                             selectedYear = year;
@@ -26,6 +28,7 @@ angular
                         $scope.selectHour = function (hours) {
                             selectedHours = hours;
                             updateSelectedDate();
+                            updateTimeHighlighting();
                         };
 
                         $scope.confirmAppointment = function () {
@@ -46,15 +49,31 @@ angular
                             var currentHighlightedDateId = currentHighlightedDate.getDate() + '-' + currentHighlightedMonthId + '-' + currentHighlightedDate.getFullYear();
 
                             if (!initHighlightedDate) {
-                                radioClassService.selectElementClass(currentHighlightedDateId, highlightedClass);
+                                radioClassService.selectElementClass(currentHighlightedDateId, highlightedDateClass);
                             } else {
                                 var newHighlightedDateId = selectedDate + '-' + selectedMonth + '-' + selectedYear;
-                                radioClassService.switchElementClass(currentHighlightedDateId, newHighlightedDateId, highlightedClass);
+                                radioClassService.switchElementClass(currentHighlightedDateId, newHighlightedDateId, highlightedDateClass);
                                 currentHighlightedDate = $scope.selectedDate;
+                            }
+                        }
+
+                        function updateTimeHighlighting() {
+                            var initHighlightedTime = currentHighlightedTime;
+                            if (!initHighlightedTime) {
+                                currentHighlightedTime = $scope.selectedDate;
+                            }
+                            var currentHighlightedTimeId = currentHighlightedTime.getHours();
+
+                            if (!initHighlightedTime) {
+                                radioClassService.selectElementClass(currentHighlightedTimeId, highlightedTimeClass);
+                            } else {
+                                radioClassService.switchElementClass(currentHighlightedTimeId, selectedHours, highlightedTimeClass);
+                                currentHighlightedTime = $scope.selectedDate;
                             }
                         }
 
                         updateSelectedDate();
                         updateCalendarHighlighting();
+                        updateTimeHighlighting();
 
                     } ]);
