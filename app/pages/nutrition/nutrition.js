9d88624171172cd8f3743b65b3846efc37e94644
Jason K <jkat@osx>
2014-08-15 09:28:14 -0700

    Added inputs for the caloric factors on the nutrition page. Added validation and correction for the inputs.

--- app/pages/nutrition/nutrition.js
+++ app/pages/nutrition/nutrition.js
@@ -3,6 +3,42 @@
 //noinspection JSUnusedLocalSymbols
 angular
         .module('BabyPlanApp', ['ngRoute', 'localStorage', 'browserSupport'])
+        .directive('ngDelay', ['$timeout', function ($timeout) {
+            return {
+                restrict: 'A',
+                scope: true,
+                compile: function (element, attributes) {
+                    var expression = attributes['ngChange'];
+                    if (!expression) {
+                        return;
+                    }
+
+                    var ngModel = attributes['ngModel'];
+                    if (ngModel) {
+                        attributes['ngModel'] = '$parent.' + ngModel;
+                    }
+                    attributes['ngChange'] = '$$delay.execute()';
+
+                    return {
+                        post: function (scope, element, attributes) {
+                            scope.$$delay = {
+                                expression: expression,
+                                delay: scope.$eval(attributes['ngDelay']),
+                                execute: function () {
+                                    var state = scope.$$delay;
+                                    state.then = Date.now();
+                                    $timeout(function () {
+                                        if (Date.now() - state.then >= state.delay) {
+                                            scope.$parent.$eval(expression);
+                                        }
+                                    }, state.delay);
+                                }
+                            };
+                        }
+                    }
+                }
+            };
+        }])
         .config(function ($routeProvider) {
             $routeProvider
                     .when('/',
@@ -17,8 +53,8 @@ angular
                     })
         })
         .controller('NutritionController',
-                [ '$scope', '$routeParams', 'localStorageService', 'browserSupportService',
-                    function ($scope, $routeParams, localStorageService, browserSupportService) {
+                [ '$scope', '$routeParams', '$timeout', 'localStorageService', 'browserSupportService',
+                    function ($scope, $routeParams, $timeout, localStorageService, browserSupportService) {
 
                         //noinspection JSUnresolvedVariable
                         var startWeek = initWeekAsNumber($routeParams.startWeek, 1);
@@ -27,6 +63,73 @@ angular
 
                         $scope.weeks = initWeeks(startWeek, endWeek);
                         $scope.ordinalTrimester = initOrdinalTrimester(startWeek);
+
+                        $scope.age = 27;
+                        $scope.ageModel = $scope.age;
+
+                        $scope.heightFeet = 5;
+                        $scope.heightFeetModel = $scope.heightFeet;
+
+                        $scope.heightInches = 4;
+                        $scope.heightInchesModel = $scope.heightInches;
+
+                        $scope.weightInPounds = 129;
+                        $scope.weightInPoundsModel = $scope.weightInPounds;
+
+                        // TODO DUPLICATION: figure out how to factor out the the xChanged() methods if possible
+                        // to remove duplication
+
+                        $scope.ageChanged = function () {
+                            var ageModel = $scope.ageModel;
+                            var isValidAge = ageModel
+                                    && !isNaN(ageModel)
+                                    && ageModel > 0
+                                    && ageModel < 100;
+                            if (!isValidAge) {
+                                ageModel = $scope.age;
+                            }
+                            $scope.age = ageModel;
+                            $scope.ageModel = ageModel;
+                        };
+
+                        $scope.heightFeetChanged = function () {
+                            var heightFeetModel = $scope.heightFeetModel;
+                            var isValidFeet = heightFeetModel
+                                    && !isNaN(heightFeetModel)
+                                    && heightFeetModel > 0
+                                    && heightFeetModel < 8;
+                            if (!isValidFeet) {
+                                heightFeetModel = $scope.heightFeet;
+                            }
+                            $scope.heightFeet = heightFeetModel;
+                            $scope.heightFeetModel = heightFeetModel;
+                        };
+
+                        $scope.heightInchesChanged = function () {
+                            var heightInchesModel = $scope.heightInchesModel;
+                            var isValidInches = heightInchesModel
+                                    && !isNaN(heightInchesModel)
+                                    && heightInchesModel >= 0
+                                    && heightInchesModel <= 12;
+                            if (!isValidInches) {
+                                heightInchesModel = $scope.heightInches;
+                            }
+                            $scope.heightInches = heightInchesModel;
+                            $scope.heightInchesModel = heightInchesModel;
+                        };
+
+                        $scope.weightInPoundsChanged = function () {
+                            var weightInPoundsModel = $scope.weightInPoundsModel;
+                            var isValidWeightInPounds = weightInPoundsModel
+                                    && !isNaN(weightInPoundsModel)
+                                    && weightInPoundsModel > 0
+                                    && weightInPoundsModel < 1000;
+                            if (!isValidWeightInPounds) {
+                                weightInPoundsModel = $scope.weightInPounds;
+                            }
+                            $scope.weightInPounds = weightInPoundsModel;
+                            $scope.weightInPoundsModel = weightInPoundsModel;
+                        };
 
                         function initWeekAsNumber(initWeek, defaultWeek) {
                             var week = initWeek;
