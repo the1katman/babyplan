4e4b40e0659c8c5d7b87efc340b6da39c0bd319a
Jason K <jkat@osx>
2014-08-19 00:49:18 -0700

    Added a changes section to the online midwife summary page.

--- app/pages/online_appointment_midwife_summary/online_appointment_midwife_summary.js
+++ app/pages/online_appointment_midwife_summary/online_appointment_midwife_summary.js
@@ -16,7 +16,22 @@ angular
                         var firstAppointmentDate = new Date(firstAppointmentDateString);
                         var dueDate = new Date();
                         dueDate.setDate(firstAppointmentDate.getDate() + (7 * 36));
-
                         $scope.dueDate = dueDate;
+
+                        var babyAppointment2Date = localStorageService.get('babyAppointment2Date');
+                        $scope.hasBabyAppointment2Date = babyAppointment2Date !== null;
+                        if ($scope.hasBabyAppointment2Date) {
+                            $scope.doctorFirstName = localStorageService.get('doctorFirstName');
+                            $scope.doctorLastName = localStorageService.get('doctorLastName');
+                            $scope.doctorCredentials = localStorageService.get('doctorCredentials');
+
+                            $scope.babyAppointment2Date = babyAppointment2Date;
+                        }
+
+                        var labAppointmentDate = localStorageService.get('labAppointmentDate');
+                        $scope.hasLabAppointmentDate = labAppointmentDate !== null;
+                        if ($scope.hasLabAppointmentDate) {
+                            $scope.labAppointmentDate = labAppointmentDate;
+                        }
 
                     } ]);
