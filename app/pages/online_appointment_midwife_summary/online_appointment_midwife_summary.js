bd3e13ae824c4356eefd20984bf0b3212db2cc6b
Jason K <jkat@osx>
2014-08-18 23:25:04 -0700

    Added dynamic content to the online appointment midwife page.

--- app/pages/online_appointment_midwife_summary/online_appointment_midwife_summary.js
+++ app/pages/online_appointment_midwife_summary/online_appointment_midwife_summary.js
@@ -1,9 +1,22 @@
 "use strict";
 
 angular
-        .module('BabyPlanApp', ['localStorage', 'browserSupport'])
+        .module('BabyPlanApp', ['localStorage', 'browserSupport', 'firstLetterCapitalize'])
         .controller('OnlineAppointmentSummaryController',
                 [ '$scope', 'localStorageService', 'browserSupportService',
                     function ($scope, localStorageService, browserSupportService) {
 
-                } ]);
+                        $scope.midwifeFirstName = localStorageService.get('midwifeFirstName');
+                        $scope.midwifeLastName = localStorageService.get('midwifeLastName');
+                        $scope.midwifeCredentials = localStorageService.get('midwifeCredentials');
+
+                        var firstAppointmentDateString = localStorageService.get('firstAppointmentDate');
+                        $scope.firstAppointmentDate = firstAppointmentDateString;
+
+                        var firstAppointmentDate = new Date(firstAppointmentDateString);
+                        var dueDate = new Date();
+                        dueDate.setDate(firstAppointmentDate.getDate() + (7 * 32));
+
+                        $scope.dueDate = dueDate;
+
+                    } ]);
